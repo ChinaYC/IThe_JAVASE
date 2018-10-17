ff6a0fb72c8d858db7a0f1f51b78565bf55b55f6
ChinaYC <1016027366@qq.com>
2018-10-17 17:47:51 +0800

    2018.10.17

---
+++ src/COM/I/JAVASE/Course.java
@@ -0,0 +1,16 @@
+package COM.I.JAVASE;
+
+/**
+ * 课程类
+*@author LYC
+**/
+
+public class Course {
+    public String id;
+    public String name;
+
+    public Course(String id,String name){
+        this.id = id;
+        this.name = name;
+    }
+}
