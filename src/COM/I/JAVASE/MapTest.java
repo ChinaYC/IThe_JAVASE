316e50fd9456224f8bea120d45df96dd84e00e49
ChinaYC <1016027366@qq.com>
2018-10-25 18:40:57 +0800

    2018.10.25

--- src/COM/I/JAVASE/MapTest.java
+++ src/COM/I/JAVASE/MapTest.java
@@ -1,4 +1,168 @@
-package COM.I.JAVASE;
+package COM.I.JAVASE
 
-public class MapTest {
+import java.util.HashMap
+import kotlin.collections.Map.Entry
+import java.util.Scanner
+
+/**
+ * 使用Map对学生类的增删改查
+ */
+class MapTest {
+
+    /**
+     * 用来承装学生类型对象
+     * Map<key></key>,value>
+     * @param args
+     */
+    var students: MutableMap<String, Student>
+
+    /**
+     * 添加构造方法
+     * 在构造器中初始化students属性
+     * @param args
+     */
+    init {
+        this.students = HashMap()
+    }
+
+    /**
+     * 测试添加:输入学生ID，判断是否被占用
+     * 若魏碑占用,则输入姓名，创建新学生对象，
+     * 并且添加到students中
+     * @param args
+     */
+    fun testPut() {
+        //创建一个Scanner对象,用来获取输入的学生ID和姓名
+        val console = Scanner(System.`in`)
+        var i = 0
+        while (i < 3) {
+            println("请输入学生ID:")
+            val ID = console.next()
+            //判断该ID是否已被占用
+            val st = students[ID]
+            if (st == null) {
+                //提示输入学生姓名
+                println("请输入学生姓名")
+                val name = console.next()
+
+                //创建新的学生对象
+                val newStudent = Student(ID, name)
+
+                //通过调用students的·put方法，添加ID-学生映射
+                students[ID] = newStudent
+                println("成功添加学生" + students[ID].name)
+                i++
+            } else {
+                println("该学生ID已被占用！")
+                continue
+            }
+        }
+    }
+
+    /**
+     * 测试Map的keySet方法
+     * @param args
+     */
+    fun testKeySet() {
+        //通过KeySet方法，返回Map中的所有"键"的set集合
+        val studentsKeySet = students.keys
+        //取得stuents的容量
+        println("总共有学生：" + students.size)
+
+        //遍历KeySet,取得每一个键,再调用get方法取得每个键对应的value
+        for (stuId in studentsKeySet) {
+            val st = students[stuId]
+            if (st != null) {
+                println("学生:" + st.name)
+            }
+        }
+    }
+
+    /**
+     * 通过entrySet方法来遍历Map
+     * @param args
+     */
+    fun testEntrySet() {
+        //通过entrySet方法,返回Map中的所有键值对
+        val entrySet = students.entries
+        for ((key, value) in entrySet) {
+            println("取得键:$key")
+            println("对应的值为:" + value.name)
+        }
+    }
+
+    /**
+     * 测试删除Map中的映射
+     * @param args
+     */
+    fun testRemove() {
+        val console = Scanner(System.`in`)
+
+        while (true) {
+            println("请输入要删除的学生ID")
+            //获取从键盘输入的待删除的学生ID
+            val ID = console.next()
+
+            //判定该ID是否有对应的学生对象
+            val st = students[ID]
+            if (st == null) {
+                //提示ID情况(此处为不存在)
+                println("该ID不存在")
+                continue
+            }
+            students.remove(ID)
+            println("成功删除学生" + st.name)
+            break
+
+        }
+    }
+
+    /**
+     * 利用put方法修改Map中的已有映射
+     * @param args
+     */
+    fun testModify() {
+        println("请输入要修改的学生ID")
+
+        //创建一个Scanner对象，获取键盘上输入的学生ID字符串
+        val console = Scanner(System.`in`)
+        while (true) {
+            //取得键盘上输入的学生ID
+            val stuID = console.next()
+
+            //从students中查找是否有相应的映射(该学生ID对应的学生对象)
+            val student = students[stuID]
+            if (stuID == null) {
+                println("该ID不存在")
+                continue
+            }
+
+            //提示当前对应的学生对象的姓名
+            println("该ID下的学生" + student.name + "将修改改为?")
+            //取得键盘上输入的学生name
+            val stuName = console.next()
+            val newStudent = Student(stuID, stuName)
+            //添加入students的map中
+            students[stuID] = newStudent
+
+            println(student.name + "成功修改为:" + students[stuID].name)
+
+            //别忘记跳出循环!!!
+            break
+        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val mp = MapTest()
+            mp.testPut()
+            mp.testKeySet()
+            mp.testRemove()
+            mp.testEntrySet()
+            mp.testModify()
+            mp.testEntrySet()
+        }
+    }
 }
--- src/COM/I/JAVASE/SetTest.java
+++ src/COM/I/JAVASE/SetTest.java
@@ -1,54 +1,54 @@
-package COM.I.JAVASE;
+package COM.I.JAVASE
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.ArrayList
+import java.util.Arrays
+import java.util.Scanner
 
-public class SetTest {
-   public List<Course> coursesToSelect;
+class SetTest {
+    var coursesToSelect: MutableList<Course>
 
-   public SetTest(){
-       //初始化coursesToSelect属性
-       coursesToSelect = new ArrayList<Course>();
-   }
+    init {
+        //初始化coursesToSelect属性
+        coursesToSelect = ArrayList()
+    }
 
     /**
      * 用于往courseToSelect中添加课程
+     *
      */
-    public void testAdd() {
+    fun testAdd() {
         //创建一个课程对象，并通过调用add方法添加到备选课程List中
-        Course cr1 = new Course("1", "数据结构");
-        coursesToSelect.add(cr1);
-        Course temp = (Course) coursesToSelect.get(0);
-//        System.out.println("添加了课程：" + temp.id + ":" + temp.name);
+        val cr1 = Course("1", "数据结构")
+        coursesToSelect.add(cr1)
+        val temp = coursesToSelect[0]
+        //        System.out.println("添加了课程：" + temp.id + ":" + temp.name);
 
-        Course cr2 = new Course("2","C语言");
-        coursesToSelect.add(0,cr2);
-        Course temp2 = (Course) coursesToSelect.get(0);
-//        System.out.println("添加了课程：" + temp2.id + ":" + temp2.name);
+        val cr2 = Course("2", "C语言")
+        coursesToSelect.add(0, cr2)
+        val temp2 = coursesToSelect[0]
+        //        System.out.println("添加了课程：" + temp2.id + ":" + temp2.name);
 
-//        System.out.println("添加了课程：" + temp.id + ":" + temp.name);
+        //        System.out.println("添加了课程：" + temp.id + ":" + temp.name);
 
-//        coursesToSelect.add(cr1);
-//        Course temp0 = (Course) coursesToSelect.get(2);
-//        System.out.println("添加了课程：" + temp.id + ":" + temp.name);
+        //        coursesToSelect.add(cr1);
+        //        Course temp0 = (Course) coursesToSelect.get(2);
+        //        System.out.println("添加了课程：" + temp.id + ":" + temp.name);
 
-//        Course cr3 = new Course("3","test");
-//        coursesToSelect.add(3,cr3);
-//        添加两门课程
-//        数组添加用 addAll(Arrays.asList());
-        Course[] course = {new Course("3","离散数学"),new Course("4","汇编语言")};
-        coursesToSelect.addAll(Arrays.asList(course));
-        Course temp3 = (Course) coursesToSelect.get(2);
-        Course temp4 = (Course) coursesToSelect.get(1);
-//        System.out.println(temp3.id+temp3.name+temp4.id+temp4.name);
+        //        Course cr3 = new Course("3","test");
+        //        coursesToSelect.add(3,cr3);
+        //        添加两门课程
+        //        数组添加用 addAll(Arrays.asList());
+        val course = arrayOf(Course("3", "离散数学"), Course("4", "汇编语言"))
+        coursesToSelect.addAll(Arrays.asList(*course))
+        val temp3 = coursesToSelect[2]
+        val temp4 = coursesToSelect[1]
+        //        System.out.println(temp3.id+temp3.name+temp4.id+temp4.name);
 
-        Course[] course2 = {new Course("5","离散数学"),new Course("6","大英")};
-        coursesToSelect.addAll(2,Arrays.asList(course2));
-        Course temp5 = (Course) coursesToSelect.get(2);
-        Course temp6 = (Course) coursesToSelect.get(3);
-//        System.out.println(temp5.id+temp5.name+temp6.id+temp6.name);
+        val course2 = arrayOf(Course("5", "离散数学"), Course("6", "大英"))
+        coursesToSelect.addAll(2, Arrays.asList(*course2))
+        val temp5 = coursesToSelect[2]
+        val temp6 = coursesToSelect[3]
+        //        System.out.println(temp5.id+temp5.name+temp6.id+temp6.name);
 
     }
 
@@ -56,53 +56,57 @@ public class SetTest {
      * 通过for each方法访问集合元素
      * @param args
      */
-    public void testForEach(){
-        System.out.println("有如下课程待选(通过for each)");
-//        for (Course cr:coursesToSelect){
-//            System.out.println("课程"+cr.id+cr.name);
-//        }
-            for (Object obj:coursesToSelect){
-                Course cr = (Course) obj;
-                System.out.println("课程"+cr.id+cr.name);
-            }
+    fun testForEach() {
+        println("有如下课程待选(通过for each)")
+        //        for (Course cr:coursesToSelect){
+        //            System.out.println("课程"+cr.id+cr.name);
+        //        }
+        for (obj in coursesToSelect) {
+            println("课程" + obj.id + obj.name)
+        }
     }
 
-    public static void main(String[] args) {
-        SetTest st = new SetTest();
-        st.testAdd();
-        st.testForEach();
+    fun testForEachForSet(st: Student) {
+        //打印输出学生所选课程!
+        println("选择的课程数量：" + st.courses.size)
+        for (cr in st.courses) {
+            println("学生选择课程:" + cr.id + cr.name)
+        }
+    }
 
-        //创建一个学生对象
-        Student student = new Student("1","小明");
-        System.out.println("欢迎学生"+student.name+"选课");
+    companion object {
 
-        //创建一个Scanner对象，用来接收从键盘输入的课程ID
-        Scanner console = new Scanner(System.in);
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val st = SetTest()
+            st.testAdd()
+            st.testForEach()
 
-        /**
-         * 获取3个键盘输入的值
-         * 如果和课程ID相同的就记录到set<Course> courses中(学生所选课程)
-         */
-        for (int i=0;i<3;i++){
-            System.out.println("请输入课程ID");
-            String courseId = console.next();
-            for (Course cr:st.coursesToSelect){
-                if(cr.id.equals(courseId)){
-                    student.courses.add(cr);
-                    //set重复数据↓(无论添加多少次都只会保留一个该对象)
-                    //而且保留的是第一次添加的那个
-                  //有毒→  student.courses.add(null);
+            //创建一个学生对象
+            val student = Student("1", "小明")
+            println("欢迎学生" + student.name + "选课")
+
+            //创建一个Scanner对象，用来接收从键盘输入的课程ID
+            val console = Scanner(System.`in`)
+
+            /**
+             * 获取3个键盘输入的值
+             * 如果和课程ID相同的就记录到set<Course> courses中(学生所选课程)
+            </Course> */
+            for (i in 0..2) {
+                println("请输入课程ID")
+                val courseId = console.next()
+                for (cr in st.coursesToSelect) {
+                    if (cr.id == courseId) {
+                        student.courses.add(cr)
+                        //set重复数据↓(无论添加多少次都只会保留一个该对象)
+                        //而且保留的是第一次添加的那个
+                        //有毒→  student.courses.add(null);
+                    }
                 }
             }
-        }
 
-        st.testForEachForSet(student);
-    }
-        public void testForEachForSet(Student st){
-            //打印输出学生所选课程!
-            System.out.println("选择的课程数量："+st.courses.size());
-            for (Course cr:st.courses){
-                System.out.println("学生选择课程:"+cr.id+cr.name);
-            }
+            st.testForEachForSet(student)
         }
+    }
 }
