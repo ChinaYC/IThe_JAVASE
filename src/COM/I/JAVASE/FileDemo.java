2c633a4b80a403fe540efa7ebc9686255e6ece78
ChinaYC <1016027366@qq.com>
2018-11-19 17:30:45 +0800

    java中 file类

---
+++ src/COM/I/JAVASE/FileDemo.java
@@ -0,0 +1,19 @@
+package COM.I.JAVASE;
+
+public class FileDemo {
+    /**
+     * JAVA I/O类
+     * Filer目录的操作
+     * file.mkdir()创建文件 reateNewfile
+     * file.delete() 删除文件
+     * 是否是一个目录
+     * file.isDirectory():
+     *
+     * 是否是一个文件
+     * file.isFile();
+     */
+
+    public static void main(String[] args) {
+        Sc
+    }
+}
