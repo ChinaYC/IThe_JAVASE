13101bb7c508559fe301a0011d49860b0a22328c
ChinaYC <1016027366@qq.com>
2018-12-21 11:01:06 +0800

    点坐标

---
+++ src/COM/I/JAVASE/Point.java
@@ -0,0 +1,15 @@
+package COM.I.JAVASE;
+
+public class CreateObjectDemo {
+    class Point(){
+        public int x = 0; //声明int类型X(点的横坐标)
+        public int y = 0; //声明int类型Y(点的纵坐标)
+
+        //构造器
+        public Point(int a,int b){
+        
+        }
+    }
+
+
+}
