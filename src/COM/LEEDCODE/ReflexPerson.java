413bbf9f6b945d33794004767364b98cb0342f7e
L2\liuyi <1016027366@qq.com>
2019-01-11 23:46:20 +0800

    no message

--- src/COM/LEEDCODE/ReflexPerson.java
+++ src/COM/LEEDCODE/ReflexPerson.java
@@ -1,4 +1,12 @@
 package COM.LEEDCODE;
 
 public class ReflexPerson {
+    public ReflexPerson(){
+        System.out.println("无参");
+    }
+
+    @Override
+    public String toString(){
+        return "中国人";
+    }
 }
