4c839c3998731771d9130210fea9c32b64f1202b
L2\liuyi <1016027366@qq.com>
2019-01-18 21:49:18 +0800

    notih

--- src/COM/LEEDCODE/FrameOne.java
+++ src/COM/LEEDCODE/FrameOne.java
@@ -1,4 +1,42 @@
 package COM.LEEDCODE;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class FrameOne {
+    public static void main(String[] args) {
+//        new AppFrame();
+    }
+
+    class AppFrame extends JFrame{
+//        JTextField in = new JTextField(10);
+//        JButton btn = new JButton("求平方");
+//        JLabel out = new JLabel("用于显示结果的标签");
+//
+//        public  AppFrame(){
+//            //设置流式布局
+//            setLayout(new FlowLayout());
+//            getContentPane().add(in);
+//            getContentPane().add(btn);
+//            getContentPane().add(out);
+//            btn.addAncestorListener( new BtnActionAdapter());
+//            setSize(400,100);
+//            //窗口关闭的语句
+//            setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+//            setVisible(true);
+//        }
+
+//        //事件监听对象
+//        class BtnActionAdapter implements ActionListener{
+//
+//            public
+//
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//
+//            }
+//        }
+    }
 }
