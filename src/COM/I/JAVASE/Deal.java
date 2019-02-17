80f5a43d6d1b951b2eee13426b9d786f22dd2778
L2 <1016027366@qq.com>
2019-02-17 23:11:06 +0800

    53牌

--- src/COM/I/JAVASE/Deal.java
+++ src/COM/I/JAVASE/Deal.java
@@ -1,4 +1,14 @@
 package COM.I.JAVASE;
 
 public class Deal {
+    public static void main(String[] args) {
+        int numHands = Integer.parseInt(args[1]);//声明整形变量，代表玩家数量
+        int cardsPerHand = Integer.parseInt(args[1]);//声明整型变量,代表每手牌的数量
+
+        //构建一副52张卡片的牌;
+        String[] suit = new String[]{"黑桃","红心","方块","梅花"};//代表花色
+        String[] rank = new String[]{"",""};
+
+        for(byte i = 127;;){}
+    }
 }
